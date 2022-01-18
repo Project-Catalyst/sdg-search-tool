01b9a0b59c4a6d1d10627a71f0c9aa09b832158b
Lucio Baglione <coire1@gmail.com>
2022-01-18 13:48:55 +0100

    Adjust configs.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 let publicPath = ''
 if (process.env.APP_ENV === 'production') {
-  publicPath = '/sdg-tool/'
+  publicPath = '/sdg-search-tool/'
 }
 if (process.env.APP_ENV === 'staging') {
   publicPath = '/'
